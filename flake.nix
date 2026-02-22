29fd4d7eea7f584411e02e4fb6a9eb79280f0c2b
Toxir Isakulov <isakulovtohir902@gmail.com>
2026-02-22 23:28:46 +0500

    idk

--- flake.nix
+++ flake.nix
@@ -12,109 +12,146 @@
 
     # Optional: Declarative tap management
     homebrew-core = {
-        url = "github:homebrew/homebrew-core";
-        flake = false;
+      url = "github:homebrew/homebrew-core";
+      flake = false;
     };
     homebrew-cask = {
-        url = "github:homebrew/homebrew-cask";
-        flake = false;
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask }:
-  let
-    configuration = { pkgs, config, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [
-          pkgs.tmux # just in case
-          pkgs.neofetch
-          pkgs.neovim # texting
-          pkgs.alt-tab-macos # i hate macos cmd+tab
-          pkgs.betterdisplay # scale monitor
-          pkgs.lazygit # tui for git operations
-          pkgs.zed-editor # my fav editor rn
-          pkgs.google-chrome # holy chrome
-        ];
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      nix-homebrew,
+      ...
+    }:
+    let
+      configuration =
+        { pkgs, config, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = with pkgs; [
+            tmux # just in case
+            neofetch
+            neovim # texting
+            alt-tab-macos # i hate macos cmd+tab
+            betterdisplay # scale monitor
+            lazygit # tui for git operations
+            zed-editor # my fav editor rn
+            google-chrome # holy chrome
+            pnpm
 
-       nixpkgs.config = {
-         allowUnfree = true;
-         allowBroken = true;
-         permittedInsecurePackages = [
-             "google-chrome-144.0.7559.97"
-         ];
-       };
+            direnv
+          ];
 
-        system.activationScripts.applications.text = let
-          env = pkgs.buildEnv {
-            name = "system-applications";
-            paths = config.environment.systemPackages;
-            pathsToLink = ["/Applications"];
+          nixpkgs.config = {
+            allowUnfree = true;
+            allowBroken = true;
+            permittedInsecurePackages = [
+              "google-chrome-144.0.7559.97"
+            ];
           };
-        in
-          pkgs.lib.mkForce ''
-          # Set up applications.
-          echo "setting up /Applications..." >&2
-          rm -rf /Applications/Nix\ Apps
-          mkdir -p /Applications/Nix\ Apps
-          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-          while read -r src; do
-            app_name=$(basename "$src")
-            echo "copying $src" >&2
-            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-          done
-              '';
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+          system.activationScripts.applications.text =
+            let
+              env = pkgs.buildEnv {
+                name = "system-applications";
+                paths = config.environment.systemPackages;
+                pathsToLink = [ "/Applications" ];
+              };
+            in
+            pkgs.lib.mkForce ''
+              # Set up applications.
+              echo "setting up /Applications..." >&2
+              rm -rf /Applications/Nix\ Apps
+              mkdir -p /Applications/Nix\ Apps
+              find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+              while read -r src; do
+                app_name=$(basename "$src")
+                echo "copying $src" >&2
+                ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+              done
+            '';
 
-      homebrew = {
-        enable = true;
-        casks = [
-         "iina" # media player
-         "cloudflare-warp"
-        ];
-        onActivation.cleanup = "zap";
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          homebrew = {
+            enable = true;
+            casks = [
+              "iina" # media player
+              "cloudflare-warp"
+              "freecad"
+            ];
+            onActivation.cleanup = "zap";
+          };
+
+          system.primaryUser = "tokhir";
+          system.defaults = {
+            dock.autohide = true;
+            dock.autohide-delay = 0.0;
+          };
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      modules = [
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+
+            enableRosetta = true;
+
+            user = "tokhir";
+
+            autoMigrate = true;
+          };
+        }
+      ];
+      imports = [
+        ./astronvim.nix
+      ];
+
+      # $ darwin-rebuild build --flake .#tokhir
+      darwinConfigurations."tokhir" = nix-darwin.lib.darwinSystem {
+        modules = [ configuration ];
       };
+    }
+    // (
+      let
+        system = "aarch64-darwin";
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        formatter.${system} = pkgs.nixfmt;
+        devShells.${system}.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nixd
+            statix
+            deadnix
+            nixfmt-tree
 
-      system.primaryUser = "tokhir";
-      system.defaults = {
-        dock.autohide = true;
-        dock.autohide-delay = 0.0;
-      };
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
-    # Build darwin flake using:
-    modules = [
-      nix-homebrew.darwinModules.nix-homebrew {
-        nix-homebrew = {
-          enable = true;
-
-          enableRosetta = true;
-
-          user = "tokhir";
-
-          autoMigrate = true;
+            self.formatter.${system}
+          ];
         };
       }
-    ];
-    # $ darwin-rebuild build --flake .#tokhir
-    darwinConfigurations."tokhir" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
+    );
 }
